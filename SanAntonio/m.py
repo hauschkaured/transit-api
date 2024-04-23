71408100a74b92e459296d8a4a69a6ee1cf8ab30
hauschkaured <118323804+hauschkaured@users.noreply.github.com>
2024-04-22 21:38:14 -0400

    l

--- SanAntonio/m.py
+++
@@ -1,9 +0,0 @@
-from google.transit import gtfs_realtime_pb2
-import requests
-
-feed = gtfs_realtime_pb2.FeedMessage()
-response = requests.get('https://gtfs.org/realtime/gtfs-realtime.proto')
-feed.ParseFromString(response.content)
-for entity in feed.entity:
-  if entity.HasField('trip_update'):
-    print(entity.trip_update)
